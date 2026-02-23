50e1abe2a8cec79c71c54bd9e6e6db4d1ce05843
premkumars2023-master <premkumar.s2023@vitstudent.ac.in>
2026-02-23 10:33:21 +0530

    Update app.java

--- src/main/java/com/example/app.java
+++ src/main/java/com/example/app.java
@@ -1,15 +1,6 @@
 package com.example; 
- 
 public class App { 
-    public static void main(String[] args) { 
-        System.out.println("Hello, Maven World!"); 
- 
-int a = 10; 
-int b = 20; 
-int sum = add(a, b); 
-System.out.println("Sum of " + a + " and " + b + " is: " + sum); 
+public int add(int a, int b) { 
+return a + b; 
 } 
-public static int add(int x, int y) { 
-return x + y; 
 } 
-}
